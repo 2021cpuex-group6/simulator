783045586d1ad5df650f05ecf9b1d1157b08ce40
hn410 <hn410>
2021-10-06 08:32:08 +0900

    change file structure

--- fib/main.cpp
+++ fib/main.cpp
@@ -4,6 +4,5 @@
 
 int main(){
     AssemblyParser parser("fib.s");
-
     
 }
---
+++ fib/simulator.hpp
@@ -0,0 +1,11 @@
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+#include "parser.hpp"
+
+class AssemblySimulator{
+    public:
+        AssemblyParser parser;
+};
+
+#endif
