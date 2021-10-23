d0fc6ee2901a61bd3f3f536e753d715d7882b6dd
hn410 <hn410>
2021-10-23 16:51:29 +0900

    update some files

--- fib/test.s
+++ fib/test.s
@@ -1,5 +1,13 @@
 # テスト用
 start:
+    addi x1 x0 0 # x1 = 0
+    addi x4 x0 4 # x4 = 4
+    addi x2 x0 520  # x2 = 512 + 8 10 00001000
+    addi x3 x0 -1 # x3 = -1
+    sw x2 8(x1) # 8... 10 00001000
+    sw x3 -4(x4) # 0... 11111...
+    lw x4 4(x4) # x4 = 520
+    lw x1 0(x1) # x1 = -1
     addi x1 x0 10
     addi x2 x0 11
     mul x3 x1 x2  # x3 = 110
