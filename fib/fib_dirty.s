cecc56aa0b7ae4b2a3cc836ff2ae93ecd32f8bdc
Hn410 <hn410@>
2021-10-26 14:53:02 +0900

    change fib example

--- fib/fib_dirty.s
+++ fib/fib_dirty.s
@@ -1,14 +1,14 @@
 # こんな汚くても一応動く
 # コード自体はfib.sと同じです
- addi x1 zero 10 #10番目のフィボナッチ数を計算
+ addi %x1 zero 10 #10番目のフィボナッチ数を計算
                       nop
-    addi x5 x0 1
+    addi %x5 x0 1
  addi x3 zero 3 
     blt  x1           x3                ret
                  addi x3 zero 1
-    addi x4            zero 1
+    addi %x4            zero 1
               addi                 x2 zero 3   
-loop             :               #x2は今何番目のフィボナッチ数を計算しているか
+loop.i             :               #x2は今何番目のフィボナッチ数を計算しているか
 #わーい       add addi zero 
   
     
@@ -18,7 +18,7 @@ loop             :               #x2は今何番目のフィボナッチ数を�
  addi x4 x5 0
     beq  x2 x1 ret
                                       addi x2 x2 1
-                j    loop
+                j    loop.i
 ret     :         
 # これで完了。x1に結果が出力される
     addi x1 x5 0
