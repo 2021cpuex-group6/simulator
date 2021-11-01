f75e64b74f324b47594119d9a55e9fd4dc9ecaf4
hn410 <hn410>
2021-11-01 19:42:59 +0900

    update main

--- fib/main.cpp
+++ fib/main.cpp
@@ -14,25 +14,27 @@ int main(int argc, char* argv[]){
     // bool doAll = false; //対話型にせず全実行するか
     bool useBin = false; //バイナリを使うかアセンブリか
     bool forGUI = false; // GUI用の出力か
+    std::vector<std::string> fileNames;
 
     if(argc < 2){
         std::cout << LACK_ARGUMENT << std::endl;
-    }else if(argc >= 3){
-        int optionN = argc - 2;
-        while(optionN>0){
-            std::string option = argv[2+(--optionN)];
-            if(option == OPTION_ALL){
-                // doAll = true;
-            }else if(option == OPTION_BIN){
-                useBin = true;
-            }else if(option == OPTION_GUI){
-                forGUI = true;                
-            }
+    }
+    int optionN = 1;
+    while(optionN < argc){
+        std::string arg = argv[optionN++];
+        if(arg == OPTION_ALL){
+            // doAll = true;
+        }else if(arg == OPTION_BIN){
+            useBin = true;
+        }else if(arg == OPTION_GUI){
+            forGUI = true;                
+        }else{
+            fileNames.emplace_back(arg);
         }
     }
-    std::string fileName = argv[1];
 
-    AssemblyParser parser(fileName, useBin, forGUI);
+
+    AssemblyParser parser(fileNames, useBin, forGUI);
     AssemblySimulator simulator(parser, useBin, forGUI);
 
     InteractiveShell shell(simulator, parser, forGUI);
