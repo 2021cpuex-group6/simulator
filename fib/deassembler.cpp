b334a5cbd7473473cb3d516e57fd53a098f5bed1
hn410 <hn410>
2021-11-03 23:41:51 +0900

    complete deassemble

--- fib/deassembler.cpp
+++ fib/deassembler.cpp
@@ -1,17 +1,23 @@
 #include "assemble/deassemble.hpp"
 #include "sims/simulator.hpp"
 
+#include <string>
 #include <iostream>
+#include <fstream>
 
 static const std::string ERROR = "エラー";
+static const std::string FIRST_EXPLAINT = "ファイル入力→f, 対話型→i (デフォルト: f)";
 static const std::string EXPLAINT = "デアセンブルしたいコードをuint32(hex)で入力してください";
+static const std::string FILE_EXPLAINT = "デアセンブルしたいファイルのパスを入力してください（デフォルト: test/test.s.out）";
 static const std::string PROMPT = ">>>";
+static const std::string DEFALUT_PATH = "test/test.s.out";
+static const std::string FILE_INPUT = "f";
+static const std::string INTERACTIVE_INPUT = "i";
 
-int main(){
-    
-    std::string input;
+
+void interactive(){
     std::cout << EXPLAINT  << std::endl;
-    
+    std::string input;
     while(true){
         std::cout << PROMPT;
         std::getline(std::cin, input);
@@ -25,6 +31,43 @@ int main(){
             std::cout << ERROR << std::endl;
         }
     }
+}
 
+void fileInput(){
+    std::cout << FILE_EXPLAINT << std::endl;
+    std::string input;
+    std::cout << PROMPT;
+    std::getline(std::cin, input);    
+    std::ifstream ifs(input);
+    if(!ifs){
+        std::string line;
+        while(!ifs.eof()){
+            uint32_t res = 0;
+            for (int i = 0; i < INST_BYTE_N; i++)
+            {
+                std::getline(ifs, line);
+                res |= (std::stoi(line, nullptr, 16)) << i*8;
+            }
+            Instruction inst = deassemble(res);
+            AssemblySimulator::printInstByRegInd(0, inst);
+        }
+    }
+
+}
+
+int main(){
+    
+    std::string input;
+    std::cout << FIRST_EXPLAINT << std::endl;
+    while(true){
+        std::cout << PROMPT;
+        std::getline(std::cin, input);
+        if(input == INTERACTIVE_INPUT){
+            interactive();
+        }else if(input == FILE_INPUT || input == ""){
+            fileInput();
+            break;
+        }
+    }
 
 }
