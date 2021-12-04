825a1c8ef3060330d81895acb5a4757ae04fe161
fushigu <fushigu5@gmail.com>
2021-12-04 16:44:40 +0900

    change test file and fix Makefile
    アセンブラのオプションが違う

--- sim/test/test_arith.s
+++ sim/test/test_arith.s
@@ -1,76 +1,172 @@
 start:
-    nop
+    addi x0 x0 0
     addi x1 x0 1
     addi x2 x0 2
     addi x3 x0 3
     addi x4 x0 4
     addi x5 x0 5
-    nop
-    nop
-    nop
-    nop
-    andi x6 x3 2
-    nop
-    nop
-    nop
-    nop
-    ori x7 x3 3
-    nop
-    nop
-    nop
-    nop
-    xori x8 x0 3
-    nop
-    nop
-    nop
-    nop
-    add x9 x2 x3
-    nop
-    nop
-    nop
-    nop
-    sub x10 x2 x3
-    nop
-    nop
-    nop
-    nop
-    and x11 x2 x3
-    nop
-    nop
-    nop
-    nop
-    or x12 x3 x0
-    nop
-    nop
-    nop
-    nop
-    xor x13 x4 x5
-    nop
-    nop
-    nop
-    nop
-    sll x14 x1 x2
-    nop
-    nop
-    nop
-    nop
-    srl x15 x10 x1
-    nop
-    nop
-    nop
-    nop
-    sra x16 x10 x1
-    nop
-    nop
-    nop
-    nop
-    slt x17 x10 x3
-    nop
-    nop
-    nop
-    nop
-    sltu x18 x10 x3
-    nop
-    nop
-    nop
-    nop    
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    andi x6 x3 2 # 3 and 2
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    ori x7 x3 3 # 3 or 3
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    xori x8 x0 3 # 3 xor 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    add x9 x2 x3 # 2 + 3
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    sub x10 x2 x3 # 2 - 3
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    and x11 x2 x3 # 2 and 3
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    or x12 x3 x0 # 3 or 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    xor x13 x4 x5 # 4 xor 5
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    sll x14 x1 x2 # 1 << 2
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    srl x15 x10 x1 # -1 >> 1
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    sra x16 x10 x1 # -1 >> 1
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    slt x17 x10 x3 # ? unsigned(-1) < 3
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    sltu x18 x10 x3 # ? signed(-1) < 3
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
+    addi x0 x0 0
