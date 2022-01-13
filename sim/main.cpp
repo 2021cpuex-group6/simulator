f9889dcad93598ebb11c931fbfe59e1f2b5f834d
hn410 <hn410>
2022-01-13 09:53:07 +0900

    change param

--- sim/main.cpp
+++ sim/main.cpp
@@ -25,40 +25,39 @@ void searchParameters(AssemblyParser &parser, const bool &useBin, MMIO &mmio){
     uint32_t optOffset[2] = {7, 5};
     uint64_t optHitN[2] = {704070462,   176076202};
 
-    uint32_t wayListN = 1;
-    uint32_t wayList[wayListN] = {1};
+    uint32_t wayListN = 2;
+    uint32_t wayList[wayListN] = {2, 4};
 
     bool first = true;
     uint64_t accessN[2] = {0, 0};
     for(uint32_t way: wayList){
-        for(uint32_t tag = tagMin; tag < (REGISTER_BIT_N - offsetMin -1); tag++){
-            for(uint32_t offset = offsetMin; offset < (REGISTER_BIT_N - tag - 1); offset++){
-                std::cout << "tag数: " << tag << "way数: " << way << " offset数: " << offset << std::endl;
-                
-                AssemblySimulator simulator(parser, useBin, false, mmio, way, offset, tag);
-                simulator.launch(false);
-                if(first){
-                    first = false;
-                    accessN[Cache::READ] = simulator.cache.hitN[Cache::READ] + simulator.cache.initMissN[Cache::READ] + simulator.cache.otherMissN[Cache::READ];
-                    accessN[Cache::WRITE] = simulator.cache.hitN[Cache::WRITE] + simulator.cache.initMissN[Cache::WRITE] + simulator.cache.otherMissN[Cache::WRITE];
-                }
-                for(int i = 0; i < Cache::TYPES_N; i++){
-                    uint32_t hitN = (simulator.cache.hitN[i]);
-                    if(optHitN[i] < hitN){
-                        // 最適値更新
-                        optWay[i] = way;
-                        optTag[i] = tag;
-                        optOffset[i] = offset;
-                        optHitN[i] = hitN;
-
-                        std::cout << (i == Cache::READ ? "読み" : "書き") << "最適値更新" << std::endl;
-                        std::cout << "  " << hitN << std::endl;
-
-                    }
-
-                }
-                
+        uint32_t tag = way == 2 ? tagMin : tagMin + 1;
+        for(uint32_t offset = offsetMin; offset < (REGISTER_BIT_N - tag - 1); offset++){
+            std::cout << "tag数: " << tag << "way数: " << way << " offset数: " << offset << std::endl;
+            
+            AssemblySimulator simulator(parser, useBin, false, mmio, way, offset, tag);
+            simulator.launch(false);
+            if(first){
+                first = false;
+                accessN[Cache::READ] = simulator.cache.hitN[Cache::READ] + simulator.cache.initMissN[Cache::READ] + simulator.cache.otherMissN[Cache::READ];
+                accessN[Cache::WRITE] = simulator.cache.hitN[Cache::WRITE] + simulator.cache.initMissN[Cache::WRITE] + simulator.cache.otherMissN[Cache::WRITE];
             }
+            for(int i = 0; i < Cache::TYPES_N; i++){
+                uint32_t hitN = (simulator.cache.hitN[i]);
+                if(optHitN[i] < hitN){
+                    // 最適値更新
+                    optWay[i] = way;
+                    optTag[i] = tag;
+                    optOffset[i] = offset;
+                    optHitN[i] = hitN;
+
+                    std::cout << (i == Cache::READ ? "読み" : "書き") << "最適値更新" << std::endl;
+                    std::cout << "  " << hitN << std::endl;
+
+                }
+
+            }
+            
         }
     }
 
@@ -74,20 +73,21 @@ void searchParameters(AssemblyParser &parser, const bool &useBin, MMIO &mmio){
 // 与えたパラメータでの性能をチェックする
 void paramCheck(AssemblyParser &parser, const bool &useBin, MMIO &mmio){
 // パラメタの下限
-    constexpr uint32_t offsetMin = 2; 
+    constexpr uint32_t offsetMin = 4; 
     constexpr uint32_t tagMin = 14;
     // 最適値が入る
 
-    int paramN = 5;
-    uint32_t ways[paramN] = {1, 1, 2, 2, 4};
-    uint32_t offsets[paramN] = {7, 5, 10, 4, 10};
+    int paramN = 4;
+    uint32_t tags[paramN] = {tagMin, tagMin, tagMin + 1, tagMin + 1};
+    uint32_t ways[paramN] = {1, 1, 2, 2};
+    uint32_t offsets[paramN] = {7, 5, 9, 10};
 
     bool first = true;
     uint64_t accessN[2] = {0, 0};
     for(int i = 0; i < paramN; i++){
-        std::cout << "tag数: " << tagMin << " way数: " << ways[i] << " offset数: " << offsets[i] << std::endl;
+        std::cout << "tag数: " << tags[i] << " way数: " << ways[i] << " offset数: " << offsets[i] << std::endl;
         
-        AssemblySimulator simulator(parser, useBin, false, mmio, ways[i], offsets[i], tagMin);
+        AssemblySimulator simulator(parser, useBin, false, mmio, ways[i], offsets[i], tags[i]);
         simulator.launch(false);
         if(first){
             first = false;
@@ -97,7 +97,7 @@ void paramCheck(AssemblyParser &parser, const bool &useBin, MMIO &mmio){
         for(int j = 0; j < Cache::TYPES_N; j++){
             uint32_t hitN = (simulator.cache.hitN[j]);
             std::cout << (j == Cache::READ ? " 読み" : " 書き") << std::endl;
-            std::cout << " ヒット数:" << hitN << ", ミス率: " << (accessN[j] - hitN) / (double)accessN[j] << std::endl;
+            std::cout << " ヒット数:" << hitN << ", ミス率: " << (accessN[j] - hitN) / (double)accessN[j] * 100 << "%" << std::endl;
 
         }
     }
