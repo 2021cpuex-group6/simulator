d89b2d0cf3241a11b70f4b72655fa6d86a891fbe
hn410 <hn410>
2022-02-12 13:46:08 +0900

    change error handling

--- sim/sims/simulator.cpp
+++ sim/sims/simulator.cpp
@@ -962,7 +962,7 @@ void AssemblySimulator::launchError(const std::string &message)const{
     }else{
         printRegisters(NumberBase::HEX, false, true);
     }
-    // throw SimException(linePair.first + ": " + std::to_string(linePair.second) + "行目:" + message);
+    throw SimException(linePair.first + ": " + std::to_string(linePair.second) + "行目:" + message);
 }
 void AssemblySimulator::launchWarning(const std::string &message)const{
     if(onWarning && (!forGUI)){
